27be57362203d5a502e9dbc5e3e595a2f4426a54
marios <marios.cako@gmail.com>
2019-01-09 04:49:20 +0200

    Create config.js

---
+++ project/configuration/config.js
@@ -0,0 +1,10 @@
+module.exports={
+	"facebook_api_key"      :     "584287812001100",
+  	"facebook_api_secret"   :     "da1e45ddd8ff5216a13bb94da1473819",
+  	"callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  	"use_database"          :     "true",
+  	"host"                  :     "localhost",
+  	"username"              :     "root",
+  	"password"              :     "******",
+  	"database"              :     "project"
+}
