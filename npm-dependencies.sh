401752ac056ccad4ff3d44feb60e728f667f9cbd
Marios <marios.cako@gmail.com>
2019-01-13 21:57:32 +0200

    npm modules need to run project

---
+++ npm-dependencies.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+npm install --save express
+npm install --save body-parser
+npm install --save bcrypt
+npm install --save mysql
+npm install --save express-session
+npm install --save cookie-parser
+npm install --save passport
+npm install --save passport-facebook
+npm install --save ejs
