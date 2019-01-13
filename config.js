0409ed0152f8c56eb9b97d0df39b7451d5ca6903
Marios <marios.cako@gmail.com>
2019-01-13 22:27:16 +0200

    Template configuration

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"facebook_api_key"      :     "584287812001100",
+  	"facebook_api_secret"   :     "da1e45ddd8ff5216a13bb94da1473819",
+  	"callback_url_log"      :     "http://localhost:3000/auth/facebook/callback",
+  	"callback_url_reg"      :     "http://localhost:3000/reg/facebook/callback",
+  	"use_database"          :     "true",
+  	"host"                  :     "localhost",
+  	"username"              :     "root",
+  	"password"              :     "your-psw",
+  	"database"              :     "project"
+}
