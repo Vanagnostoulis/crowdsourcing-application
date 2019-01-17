7f5318e0edbff5df639b7b9de1e8e810399f1bc2
lorki <pa.koromilas@gmail.com>
2019-01-18 00:29:58 +0200

    Allagh gia na ginetai login kai mesw username(oxi mono mail)

--- project/node/login.js
+++ project/node/login.js
@@ -1,18 +1,26 @@
 module.exports = function(app, con, bcrypt) {
 /* post method -- request and respond */
 /* catch login submission */
-app.post('/login', function (req, res) { 
+app.post('/login', function (req, res) {
   /* variables for login */
-  
-  username = req.body.uname; /* name = "uname" in javascript on some placeholder */ 
+
+  username = req.body.uname; /* name = "uname" in javascript on some placeholder */
   password = req.body.psw;
-  
-  //to compare password that user supplies in the future
-  var arr = username.split('@')
-  var sql = "SELECT Password, Username FROM Users Where " + 
-  		"Domain_Id = (Select Domain_Id from Email_Domains where Domain_Name = '" + arr[1] + "')" + 
-  		"and Local_Part = '" + arr[0] + "'";
-  
+
+  mail_login = false;
+
+  if (username.includes("@")) {
+    mail_login = true;
+    //to compare password that user supplies in the future
+    var arr = username.split('@')
+    var sql = "SELECT Password, Username FROM Users Where " +
+        "Domain_Id = (Select Domain_Id from Email_Domains where Domain_Name = '" + arr[1] + "')" +
+        "and Local_Part = '" + arr[0] + "'";
+  }
+  else{
+      var sql = "SELECT Password FROM Users Where  Username = '" + username + "'";
+  }
+
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log(result[0])
@@ -20,13 +28,16 @@ app.post('/login', function (req, res) {
     	res.render('index', {authentication_failed: "Το όνομα χρήστη δεν ταιριάζει με το συνθηματικό.",
     						 password_mismatch: null,
     						 error_user_exist: null
-    				});	
+    				});
     }
     else {
     	hash = result[0].Password
     	bcrypt.compare(password, hash, function(err, doesMatch){
   			if (doesMatch){
-  	 			res.render('user', {username: result[0].Username});
+          if (mail_login) 
+            res.render('user', {username: result[0].Username});
+          else
+            res.render('user', {username: username});
   			}else{
      			res.render('index', {authentication_failed: "Το όνομα χρήστη δεν ταιριάζει με το συνθηματικό.",
     						 password_mismatch: null,
@@ -37,8 +48,8 @@ app.post('/login', function (req, res) {
   	}
    	console.log("username checked.");
   });
-   
-  
+
+
   console.log("Entered to login");
   console.log("username: " + username);
   console.log("password: " + password);
