c8b95333115a06f627a67d6b5ffb9d7cb394f243
eleanna <eleannaval1996@gmail.com>
2019-03-05 23:05:56 +0200

    final

---
+++ project/npm-dependencies.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+npm install --save nodemailer
+npm install --save express
+npm install --save async
+npm install --save body-parser
+npm install --save bcrypt
+npm install --save mysql
+npm install --save express-session
+npm install --save cookie-parser
+npm install --save passport
+npm install --save passport-facebook
+npm install --save ejs
+npm install --save request
+npm install --save request-promise
